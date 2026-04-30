22831e155c38279f92184bf3d192461a1b0bb893
Marcosjv1 <marcos.javier.vidal@gmail.com>
2026-04-30 13:12:59 -0600

    Update 12-log_stats.py

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -8,18 +8,15 @@ def log_stats():
     client = MongoClient('mongodb://127.0.0.1:27017')
     collection = client.logs.nginx
 
-    # Total de logs
     n_logs = collection.count_documents({})
     print("{} logs".format(n_logs))
 
-    # Estadísticas por método
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
         count = collection.count_documents({"method": method})
         print("\tmethod {}: {}".format(method, count))
 
-    # Conteo específico de status check
     status_check = collection.count_documents({"method": "GET", "path": "/status"})
     print("{} status check".format(status_check))
 
