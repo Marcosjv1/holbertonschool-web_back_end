24e0965b4e812ea649d9b5c3003f36d959ed440b
Marcosjv1 <marcos.javier.vidal@gmail.com>
2026-04-30 13:44:03 -0600

    Create 1-country_users.sql

---
+++ 0x0C-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,7 @@
+-- Script que crea una tabla 'users' con el campo country como ENUM
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
