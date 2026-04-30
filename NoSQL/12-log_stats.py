94f4cb167bda90512f0725209f4fd1267bea1530
Marcosjv1 <marcos.javier.vidal@gmail.com>
2026-04-30 13:04:35 -0600

    Refactor log_stats to use a single collection variable

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -6,23 +6,21 @@ from pymongo import MongoClient
 def log_stats():
     """ Proporciona estadísticas sobre los logs de Nginx """
     client = MongoClient('mongodb://127.0.0.1:27017')
-    nginx_collection = client.logs.nginx
+    collection = client.logs.nginx
 
     # Total de logs
-    total = nginx_collection.count_documents({})
-    print("{} logs".format(total))
+    n_logs = collection.count_documents({})
+    print("{} logs".format(n_logs))
 
     # Estadísticas por método
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
-        count = nginx_collection.count_documents({"method": method})
+        count = collection.count_documents({"method": method})
         print("\tmethod {}: {}".format(method, count))
 
-    # Conteo específico de status check
-    status_check = nginx_collection.count_documents(
-        {"method": "GET", "path": "/status"}
-    )
+    # Estado de check
+    status_check = collection.count_documents({"method": "GET", "path": "/status"})
     print("{} status check".format(status_check))
 
 
